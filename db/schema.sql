1b716ba0d772a13dda4d7f32f06712a02a569914
moliveros <mcoliveros1202@gmail.com>
2022-07-13 19:44:01 -0500

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY;
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    job_title VARCHAR(30) NOT NULL,
+    -- department VARCHAR(30) NOT NULL,
+    -- salary DECIMAL
+);
