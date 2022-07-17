1c3743c81ccd6d70402cf5a395d6661910cc5415
moliveros <mcoliveros1202@gmail.com>
2022-07-17 17:32:32 -0500

    created functions to build database call, get a single department, create a department, or delete a department

--- server.js
+++ server.js
@@ -26,15 +26,35 @@ db.query(`SELECT * FROM department`, (err, rows) => {
     console.log(rows);
 });
 
-// Delete a department
-db.query(`DELETE FROM department WHERE id = ?`, 1, (err, result) {
-if (err) {
-    console.log(err);
-}
-console.log(result);
-})
+// GET a single candidate
+db.query(`SELECT * FROM department WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
 
-// general error message
+// Create a department
+const sql = `INSERT INTO department (id, department_name)
+VALUES (?, ?)`;
+const params = [1, 'Marketing'];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+// Delete a department
+// db.query(`DELETE FROM department WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// default error message (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
